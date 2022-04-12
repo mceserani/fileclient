67b4722f119e370ebc60441470a84c1b0b1ff2e5
Matteo Ceserani <ceserani@gmail.com>
2022-04-12 12:55:36 +0200

    First commit

---
+++ src/main/java/it/edu/polomanettiporciatti/fileclient/FileClient.java
@@ -0,0 +1,18 @@
+package it.edu.polomanettiporciatti.fileclient;
+
+public class FileClient {
+	
+	private FileClient(){
+	}
+
+	/**
+	 * 
+	 * @param args Command line arguments
+	 * @author 
+	 * @version 
+	 */
+    public static void main(String[] args){
+       
+    }
+
+}
---
+++ src/test/java/it/edu/polomanettiporciatti/fileclient/FileClientTest.java
@@ -0,0 +1,20 @@
+package it.edu.polomanettiporciatti.fileclient;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Unit test for FileClient.
+ */
+class FileClientTest {
+
+    /**
+     * Rigorous Test.
+     */
+    @Test
+    void testFileClient() {
+        assertEquals(1, 1);
+    }
+
+}
